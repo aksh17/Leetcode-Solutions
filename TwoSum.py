90f36ba993aad12031e0a469d7a0fe398c7c4ee9
Akshaya Prabakaran <43679591+aksh17@users.noreply.github.com>
2020-02-28 23:01:32 -0800

    Two Sum

--- TwoSum.py
+++ TwoSum.py
@@ -5,7 +5,8 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        result ={}
+        for ind,ele in enumerate(nums):
+            if target - ele in result:
+                return [result[target-ele],ind]
+            result[ele]=ind
